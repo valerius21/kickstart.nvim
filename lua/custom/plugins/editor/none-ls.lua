44791db525bbab104978b999320f13c6ec8ae6a8
Valerius Mattfeld <mail@valerius.me>
2024-05-04 02:17:53 +0200

    configure none-ls to use biome

--- lua/custom/plugins/editor/none-ls.lua
+++ lua/custom/plugins/editor/none-ls.lua
@@ -1,13 +1,20 @@
 return {
   {
     'nvimtools/none-ls.nvim',
-    ft = { 'go' },
+    ft = { 'go', 'typescript', 'typescriptreact', 'javascript', 'json' },
     opts = function()
       local null_ls = require 'null-ls'
       local opts = {
         sources = {
           null_ls.builtins.formatting.gofumpt,
-          null_ls.builtins.formatting.goimports,
+          null_ls.builtins.formatting.goimports_reviser,
+          null_ls.builtins.formatting.biome.with {
+            args = {
+              'check',
+              '--apply-unsafe',
+              '--stdin-file-path=$FILENAME',
+            },
+          },
         },
       }
       return opts
