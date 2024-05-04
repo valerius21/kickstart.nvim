b0fc4279ffeb94181d6aa23efbfe7cd6ff6e7335
Valerius Mattfeld <mail@valerius.me>
2024-05-05 01:16:26 +0200

    refactor none-ls for typescript location

--- lua/custom/plugins/editor/none-ls.lua
+++ lua/custom/plugins/editor/none-ls.lua
@@ -6,8 +6,6 @@ return {
       local null_ls = require 'null-ls'
       local opts = {
         sources = {
-          -- Go
-          null_ls.builtins.formatting.golines,
           -- Typscript with React
           null_ls.builtins.formatting.biome.with {
             args = {
