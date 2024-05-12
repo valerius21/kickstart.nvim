b92357436a900ee95b4c07db770407e80293b608
Valerius Mattfeld <mail@valerius.me>
2024-05-12 13:47:09 +0200

    update copilot config

--- lua/custom/plugins/editor/trial_copilot.lua
+++ lua/custom/plugins/editor/trial_copilot.lua
@@ -1,12 +1,9 @@
 return {
-  {
-    'github/copilot.vim',
-    config = function()
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false,
-      })
-      vim.g.copilot_no_tab_map = true
-    end,
-  },
+  -- TODO: awaiting configuration
+  -- {
+  --   'zbirenbaum/copilot.lua',
+  --   cmd = 'Copilot',
+  --   event = 'InsertEnter',
+  --   opts = {},
+  -- },
 }
