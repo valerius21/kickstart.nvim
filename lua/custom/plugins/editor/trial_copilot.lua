4114d1140807705974660c744d5b3ddc31864148
Valerius Mattfeld <mail@valerius.me>
2024-05-12 14:46:16 +0200

    split copilot config into extra file

--- lua/custom/plugins/editor/trial_copilot.lua
+++ lua/custom/plugins/editor/trial_copilot.lua
@@ -1,9 +1,17 @@
 return {
   -- TODO: awaiting configuration
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   cmd = 'Copilot',
-  --   event = 'InsertEnter',
-  --   opts = {},
-  -- },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {},
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      --@param opts cmp.ConfigSchema
+      opts = function(_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, { name = 'copilot', group_index = 2 })
+      end,
+    },
+  },
 }
