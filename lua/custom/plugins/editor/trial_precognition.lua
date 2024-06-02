a8c20e02274a2853546072152a471d5cfbd02db7
Valerius Mattfeld <mail@valerius.me>
2024-06-02 19:50:25 +0200

    update config; remove noice, update trouble and precognition

--- lua/custom/plugins/editor/trial_precognition.lua
+++ lua/custom/plugins/editor/trial_precognition.lua
@@ -3,11 +3,6 @@ return {
     'tris203/precognition.nvim',
     event = 'VeryLazy',
     -- Run function for when a buffer is opened
-    config = function()
-      local plugin = require 'precognition'
-      plugin.setup()
-      plugin.show()
-      return plugin
-    end,
+    opts = {}
   },
 }
