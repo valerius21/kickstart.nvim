ee7e06523427a24d1060f1d42ead1e0a3aef30d5
Valerius Mattfeld <mail@valerius.me>
2025-01-06 15:01:00 +0100

    add supermaven

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 -- [[ Keybindings ]]
 -- display tabstops as two spaces
 vim.opt.tabstop = 2
@@ -12,7 +8,6 @@ vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = 'Split window vertically' })
 vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split window horizontally' })
 
 -- buffers
--- vim.keymap.set('n', '<leader>bd', '<cmd>quit<cr>', { desc = 'Close current window' })
 
 -- movement
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move half a page down' })
