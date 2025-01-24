22a07cd1a8d6322859928bc018d6fb21ce38f296
Valerius Mattfeld <mail@valerius.me>
2025-01-24 15:49:04 +0100

    update auto-sessions

--- lua/custom/plugins/editor/auto-sessions.lua
+++ lua/custom/plugins/editor/auto-sessions.lua
@@ -1,4 +1,3 @@
--- NOTE: take a look at https://github.com/folke/snacks.nvim/blob/main/docs/terminal.md in the future
 return {
   {
     'rmagatti/auto-session',
@@ -10,7 +9,9 @@ return {
       'rmagatti/auto-session',
       'nvim-telescope/telescope.nvim',
     },
-    opts = {},
+    opts = {
+      supressed_dirs = { '~/', '~/Downloads', '/' },
+    },
     keys = {
       {
         '<leader>sS',
