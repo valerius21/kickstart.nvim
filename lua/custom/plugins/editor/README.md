e959754d91d3738a6bd9226bfc84deee1b09ef75
Valerius Mattfeld <mail@valerius.me>
2025-01-24 15:16:09 +0100

    add basic avante

---
+++ lua/custom/plugins/editor/README.md
@@ -0,0 +1,5 @@
+# Editor
+
+Plugins here are for editor-related stuff.
+
+`trial_*.lua` files contain plugins where I'm trying them out, since the goal of this config is to keep it as minimal as possible.
---
+++ lua/custom/plugins/editor/trial_avante.lua
@@ -0,0 +1,45 @@
+-- https://www.youtube.com/watch?v=r-3o35-5hlg
+return {
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = '*', -- get the latest tag from the repo
+    opts = {},
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot' TODO: supermaven compatibility?
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
+}
