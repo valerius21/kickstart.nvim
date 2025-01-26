0d43d3a38e11a1a0fcc473422bacd8ddf2bd4af7
Valerius Mattfeld <mail@valerius.me>
2025-01-26 11:57:19 +0100

    update neorg and avaten config

--- lua/custom/plugins/editor/trial_avante.lua
+++ lua/custom/plugins/editor/trial_avante.lua
@@ -5,7 +5,17 @@ return {
     event = 'VeryLazy',
     lazy = false,
     version = '*', -- get the latest tag from the repo
-    opts = {},
+    opts = {
+      -- provider = 'ollama',
+      -- vendors = {
+      --   ollama = {
+      --     __inherited_from = 'openai',
+      --     api_key_name = '',
+      --     endpoint = 'http://127.0.0.1:11434/v1',
+      --     model = 'deepseek-r1:32b',
+      --   },
+      -- },
+    },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
     dependencies = {
